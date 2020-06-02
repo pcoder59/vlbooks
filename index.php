40c73312c0854be5174bfb46f55a4ce1bf0ca46b
Prem Raj <premraj59128@protonmail.com>
2020-06-02 10:18:21 +0530

    Added Bootstrap CDN Links

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VLBOOKS - We Love Books</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" type="text/css">
 </head>
 <body>
-    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" type="text/javascript"></script>
 </body>
 </html>
