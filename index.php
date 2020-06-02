50b35b77a60cb8b0112d04bcdc81e8740c83f817
Prem Raj <premraj59128@protonmail.com>
2020-06-02 10:43:34 +0530

    Fixed Bootstrap JS

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <div id="app">
         <header-section></header-section>
     </div>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous" type="text/javascript"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" type="text/javascript"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js" type="text/javascript"></script>
     <!--<script src="https://cdn.jsdelivr.net/npm/vue@2.6.11"></script>-->
 </body>
