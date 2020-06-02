5f0d21c95c6ae5b8fb5a58e20f44d5aedfaed30e
Prem Raj <premraj59128@protonmail.com>
2020-06-02 10:42:55 +0530

    Added Header, not working. Bootstrap JS needs Fix

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" type="text/css">
 </head>
 <body>
+    <div id="app">
+        <header-section></header-section>
+    </div>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous" type="text/javascript"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js" type="text/javascript"></script>
---
+++ js/script.js
@@ -0,0 +1,13 @@
+Vue.component('header-section', {
+    template: `
+    <header class="jumbotron text-center">
+        <h1>VLBOOKS</h1>
+        <p>We Love Books!</p>
+    </header>
+    `
+})
+
+var app = new Vue({
+    el: "#app",
+    data: {}
+})
