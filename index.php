aa1fe6bf4fbfb836cebda9562e21625e61cf6c2b
Prem Raj <premraj59128@protonmail.com>
2020-06-02 10:19:49 +0530

    Added JQuery CDN Link

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous" type="text/javascript"></script>
 </body>
 </html>
