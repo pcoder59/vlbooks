108a121c123cc419b418aa99636c0f6d312bf287
Prem Raj <premraj59128@protonmail.com>
2020-06-03 14:20:57 +0530

    Added Back Button on Category Page

--- js/script.js
+++ js/script.js
@@ -16,23 +16,36 @@ Vue.component('category-display', {
 
 Vue.component('categories-columns', {
     template: `
-    <section class="card-columns" v-on:click="hide" v-bind:style={display:displayCategory}>
-        <article v-for="category in categories" class="card">
-            <div class="card-body text-center">
-                <p>{{ category }}</p>
-            </div>
-        </article>
-    </section>
+    <div>
+        <section class="container" v-bind:style={display:displayBack}>
+            <article class="row">
+                <button class="btn btn-success mx-auto" v-on:click="show" id="displayBack">Back</button>
+            </article>
+        </section>
+        <section class="card-columns" v-on:click="hide" v-bind:style={display:displayCategory}>
+            <article v-for="category in categories" class="card">
+                <div class="card-body text-center">
+                    <p>{{ category }}</p>
+                </div>
+            </article>
+        </section>
+    </div>
     `,
     data: function() {
         return {
             categories: ["Action and Adventure", "Children's", "Comic Book", "Crime", "Drama", "Fantasy", "Graphic Novel", "Historical Fiction", "Horror", "Mystery", "Picture Book", "Poetry", "Political Thriller", "Romance", "Satire", "Science Fiction", "Short Story", "Suspense", "Thriller", "Young Adult", "Art", "Autobiography", "Biography", "Cookbook", "Diary", "Dictionary", "Encyclopedia", "Guide", "Health", "History", "Journal", "Math", "Memoir", "Prayer", "Religion and Spirituality", "Textbook", "Science", "Self-Help", "Travel", "True Crime"],
             displayCategory: "block",
+            displayBack: "none"
         }
     },
     methods: {
         hide: function() {
-            this.displayCategory = "none"
+            this.displayCategory = "none";
+            this.displayBack = "block";
+        },
+        show: function() {
+            this.displayBack = "none";
+            this.displayCategory = "block";
         }
     },
 })
