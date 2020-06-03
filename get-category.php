94afbb69e3d379c8c4139efbd627e55a1f6097f8
Prem Raj <premraj59128@protonmail.com>
2020-06-03 08:45:26 +0530

    Completed Get-Category PHP Script

--- get-category.php
+++ get-category.php
@@ -3,21 +3,29 @@ include("connect-sql.php");
 
 $category = $_POST["category"];
 
-try {
-    $stmt = $conn->prepare("select bookid, bookname, author, categories from books where categories like %?%");
-    $stmt->bind_param("s", $category);
-    $stmt->execute();
+$stmt = $conn->prepare("select bookid, bookname, author, categories from books where categories like %?%");
+$stmt->bind_param("s", $category);
+$stmt->execute();
 
-    $return_arr = array();
+$return_arr = array();
 
-    $result = $stmt->get_result();
-    
-    if($result->num_rows == 0) {
+$result = $stmt->get_result();
 
-    }
+if($result->num_rows == 0) {
 
-    while($row = $result->fetch_assoc()) {
-        
-    }
 }
 
+while($row = $result->fetch_assoc()) {
+    $bookid = $row['bookid'];
+    $bookname = $row['bookname'];
+    $author = $row['author'];
+    $categories = $row['categories'];
+    $categories = $categories.split(',');
+
+    $return_arr = array("bookid" => $id,
+                        "bookname" => $bookname,
+                        "author" => $author,
+                        "categories" => $categories);
+}
+
+echo json_encode($return_arr);
