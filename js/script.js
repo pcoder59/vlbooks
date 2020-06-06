bbb25767544625f1b744025fea6ff9233632e786
Prem Raj <premraj59128@protonmail.com>
2020-06-06 13:09:30 +0530

    Added Back Home Button and Removed test cats

--- js/script.js
+++ js/script.js
@@ -45,7 +45,8 @@ Vue.component('categories-columns', {
     `,
     data: function() {
         return {
-            categories: ["Action and Adventure", "Children's", "Comic Book", "Crime", "Drama", "Fantasy", "Graphic Novel", "Historical Fiction", "Horror", "Mystery", "Picture Book", "Poetry", "Political Thriller", "Romance", "Satire", "Science Fiction", "Short Story", "Suspense", "Thriller", "Young Adult", "Art", "Autobiography", "Biography", "Cookbook", "Diary", "Dictionary", "Encyclopedia", "Guide", "Health", "History", "Journal", "Math", "Memoir", "Prayer", "Religion and Spirituality", "Textbook", "Science", "Self-Help", "Travel", "True Crime"],
+            categories: ["Fantasy"],
+            //"Action and Adventure", "Children's", "Comic Book", "Crime", "Drama", "Graphic Novel", "Historical Fiction", "Horror", "Mystery", "Picture Book", "Poetry", "Political Thriller", "Romance", "Satire", "Science Fiction", "Short Story", "Suspense", "Thriller", "Young Adult", "Art", "Autobiography", "Biography", "Cookbook", "Diary", "Dictionary", "Encyclopedia", "Guide", "Health", "History", "Journal", "Math", "Memoir", "Prayer", "Religion and Spirituality", "Textbook", "Science", "Self-Help", "Travel", "True Crime"
             displayCategory: "block",
             displayBack: "none"
         }
